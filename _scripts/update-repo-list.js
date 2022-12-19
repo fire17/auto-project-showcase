9e5157056bef3e212a306d6d5cdb9dcbfef35e66
Tami <fire17@gmail.com>
2022-12-19 16:43:46 +0200

    with secret keys

--- _scripts/update-repo-list.js
+++ _scripts/update-repo-list.js
@@ -2,7 +2,9 @@ const axios = require('axios');
 const fs = require('fs');
 
 // Replace this with your personal access token
-const TOKEN = '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';
+// const TOKEN = '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';
+const TOKEN = process.env.PROJECT_SHOWCASE_KEY;
+
 
 // // Make a GET request to the GitHub API to retrieve a list of your repositories
 // axios.get('https://api.github.com/user/repos', {
