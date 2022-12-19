1e0f4cabf795ac20c8a1acf9983bf9bd17bbc2f8
Tami <fire17@gmail.com>
2022-12-19 16:17:46 +0200

    with debugging

--- _scripts/update-repo-list.js
+++ _scripts/update-repo-list.js
@@ -5,11 +5,12 @@ const fs = require('fs');
 const TOKEN = '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';
 
 // Make a GET request to the GitHub API to retrieve a list of your repositories
-axios.get('https://api.github.com/fire17/repos', {
+axios.get('https://api.github.com/user/repos', {
   headers: {
     Authorization: `Token ${TOKEN}`
   }
 }).then(response => {
+    console.error(response.data);
   // Save the list of repositories to the _data/repos.json file
   fs.writeFileSync('_data/repos.json', JSON.stringify(response.data));
 }).catch(error => {
